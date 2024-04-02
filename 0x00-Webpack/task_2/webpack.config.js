5b1426524de90d98b39f740ba20c0cf45b745d6d
amisoni70 <amisoni7014@gmail.com>
2024-04-02 17:01:14 +0300

    Part of task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+	use: ["style-loader", "css-loader"],
+      },
+      {
+	test: /\.(gif|png|jpe?g|svg)$/i,
+	type: 'asset/resource',
+      },
+    ]
+  },
+};
