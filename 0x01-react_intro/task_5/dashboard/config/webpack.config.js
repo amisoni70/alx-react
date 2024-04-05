484fc3efdab2f7420d023352c377595cf59b6c5a
amisoni70 <amisoni7014@gmail.com>
2024-04-05 23:03:23 +0300

    progress

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
   module: {
@@ -30,7 +30,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      name: 'index.html',
+      filename: 'index.html',
       template: './dist/index.html',
       inject: false,
     })
