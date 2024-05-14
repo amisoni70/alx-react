39f495663a7e57cae5fb7490741669d4389be6eb
amisoni70 <amisoni7014@gmail.com>
2024-05-14 16:07:56 +0300

    Improving the notifications reducer

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from "../actions/notificationActionTypes";
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER, SET_LOADING_STATE } from "../actions/notificationActionTypes";
 import { notificationsNormalizer } from "../schema/notifications";
 import immutable, { setIn } from 'immutable';
 
@@ -6,8 +6,9 @@ const { Map, setIn } = immutable;
 
 export const initialNotificationState = {
     notifications: [],
-    filter: 'DEFAULT'
-}
+    filter: 'DEFAULT',
+    loading: false,
+};
 
 export const notificationReducer = (state = Map(initialNotificationState), action) => {
     switch(action.type) {
@@ -15,14 +16,16 @@ export const notificationReducer = (state = Map(initialNotificationState), actio
             const data = notificationsNormalizer(action.data);
             Object.keys(data.notifications).map((key) => {
                 data.notifications[key].isRead = false;
-            })
-            return state.merge(data)
+            });
+            return state.mergeDeep(data);
         case MARK_AS_READ:
             return setIn(state, ['notifications', String(action.index), isRead], true);
         case SET_TYPE_FILTER:
-            return setIn('filter', action.filter)
+            return setIn('filter', action.filter);
+        case SET_LOADING_STATE:
+            return setIn('loading', action.loading);
         default:
             break;
     }
     return state;
-}
+};
