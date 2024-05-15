83ba6913bcf45cd12dd364f6b1b2d5bc847fde2f
amisoni70 <amisoni7014@gmail.com>
2024-05-15 11:45:48 +0300

    Task 24

--- 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.js
@@ -1,4 +1,5 @@
-import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import { SELECT_COURSE, UNSELECT_COURSE, FETCH_COURSE_SUCCESS } from "./courseActionTypes";
+import "node-fetch";
 
 export function selectCourse(index) {
     return {
@@ -6,7 +7,7 @@ export function selectCourse(index) {
         index: index,
     };
 }
-
+j
 export const boundSelectCourse = (index) => dispatch(selectCourse(index));
 
 export function unSelectCourse(index) {
@@ -18,3 +19,18 @@ export function unSelectCourse(index) {
 
 export const boundUnSelectCourse = (index) => dispatch(unSelectCourse(index));
 
+export const setCourses = (data) => {
+    return {
+      type: FETCH_COURSE_SUCCESS,
+      data,
+    };
+};
+
+export const fetchCourses = () => {
+    return (dispatch) => {
+      return fetch("./courses.json")
+        .then((res) => res.json())
+        .then((data) => dispatch(setCourses(data)))
+        .catch((error) => {});
+    };
+};
